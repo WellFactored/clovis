9bc786cb03d4cacb2f1b66f592cf060e56c7ede9
Doug Clinton <98534+DougC@users.noreply.github.com>
2019-02-10 13:34:31 +0000

    Improved the "isSecure" check to see if the request was forwarded from https

--- src/main/scala/clovis/activitypub/ActivityPubRoutes.scala
+++ src/main/scala/clovis/activitypub/ActivityPubRoutes.scala
@@ -7,17 +7,18 @@ import org.http4s.{HttpRoutes, Request}
 
 case class HostDetails(host: String, isSecure: Boolean)
 object HostDetails {
-  def from[F[_]](r: Request[F], localDomain: String): HostDetails =
-    HostDetails(r.headers.get(CaseInsensitiveString("host")).map(_.value).getOrElse(localDomain), r.isSecure.getOrElse(true))
+  def from[F[_]](r: Request[F], localDomain: String): HostDetails = {
+    val isSecure = r.isSecure.getOrElse(true) || r.headers.get(CaseInsensitiveString("X-Forwarded-Proto")).map(_.value.toLowerCase).contains("https")
+    HostDetails(r.headers.get(CaseInsensitiveString("host")).map(_.value).getOrElse(localDomain), isSecure)
+  }
 }
 
-class ActivityPubRoutes[F[_]: Sync](controller: ActivityPubController[F], localDomain:String) extends Http4sDsl[F] with MountableRoutes[F] {
+class ActivityPubRoutes[F[_]: Sync](controller: ActivityPubController[F], localDomain: String) extends Http4sDsl[F] with MountableRoutes[F] {
   override def mountPoint: String = "/"
 
   override def routes: HttpRoutes[F] =
     HttpRoutes.of[F] {
       case r @ GET -> Root / "person" / name =>
-
         controller.getPerson(name, HostDetails.from(r, localDomain))
     }
 }
